69f88d6af2853400b8f51b9875c67797341dfe56
hard02 <98350448+hard02@users.noreply.github.com>
2023-05-04 23:43:49 +0530

    Update iteration_1_parser.py
    added comments

--- iteration_1_parser.py
+++ iteration_1_parser.py
@@ -1,10 +1,13 @@
 import nltk
+
+# Import the necessary functions for tokenization and part-of-speech tagging
 from nltk import word_tokenize, pos_tag
 
-sentence = input("enter the sentence: ") 
 
-tokens = word_tokenize(sentence)
-tagged = pos_tag(tokens)
+sentence = input("enter the sentence: ")    
 
-tree = nltk.ne_chunk(tagged)
+tokens = word_tokenize(sentence)  # Tokenize the sentence into individual words
+tagged = pos_tag(tokens) # Tag each word with its part of speech
+
+tree = nltk.ne_chunk(tagged)  # Apply named entity recognition to the tagged sentence
 tree.draw()
